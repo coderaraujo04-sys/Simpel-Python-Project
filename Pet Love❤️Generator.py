aae9aa229370a192d9bb15b522e052cf7c6e1d69
coderaraujo04-sys <coderaraujo04@gmail.com>
2025-11-28 23:05:27 +0700

    First commit - Simple Python Project

---
+++ Pet Love❤️Generator.py
@@ -0,0 +1,5 @@
+print("Hello welcome to pet love generator")
+city = input("What is your your name?\n")
+pet = input("what is your pet name?\n")
+print("Keep calm and love animals " + city + "❤️" + pet)
+
